8cf5b41c33270dffdc88c8791ceb1f11feef1244
Студенческая <@rbiu.local>
2025-09-30 11:12:54 +0500

    alphagameplay

---
+++ src/data.js
@@ -0,0 +1,59 @@
+import pudge from './images/pudge_lg.jpg'
+import dawnbreaker from './images/dawnbreaker_lg.jpg'
+import antimage from './images/antimage_lg.jpg'
+import ogre_magi from './images/ogre_magi_lg.jpg'
+import skeleton_king from './images/skeleton_king_lg.jpg'
+import spirit_breaker from './images/spirit_breaker_lg.jpg'
+
+const _images = [
+    {
+        'id':0,
+        'url':pudge
+    },
+    {
+        'id':1,
+        'url':dawnbreaker
+    },
+    {
+        'id':3,
+        'url':ogre_magi
+    },
+    {
+        'id':4,
+        'url':antimage
+    },
+    {
+        'id':5,
+        'url':skeleton_king
+    },
+    {
+        'id':6,
+        'url':spirit_breaker
+    },
+    {
+        'id':7,
+        'url':pudge
+    },
+    {
+        'id':8,
+        'url':dawnbreaker
+    },
+    {
+        'id':9,
+        'url':ogre_magi
+    },
+    {
+        'id':10,
+        'url':antimage
+    },
+    {
+        'id':11,
+        'url':skeleton_king
+    },
+    {
+        'id':12,
+        'url':spirit_breaker
+    }
+];
+
+export {_images}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
-const cards = document.querySelectorAll(".card");
-for (let card of cards)
-{
+import { _images } from "./data.js";
+
+_images.sort(() => Math.random() - 0.5);
+
+let visibleCards =[];
+
+const container = document.querySelector(".container");
+_images.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("card" + item.id);
+    card.tabIndex = 0;
+
+    const img = document.createElement("img");
+    img.src = item.url;
+    img.alt = item.id;
+
+    card.append(img);
+    container.append(card);
+
     card.addEventListener("click", ()=>
     {
-        card.classList.add("card-show");
+        if(visibleCards.length<2){
+            card.classList.add("card-show");
+            visibleCards.push(item);
+            if(visibleCards.length == 2){
+                setTimeout(() =>{
+                    card.classList.remove("card-show");
+                    document.querySelector(".card"+visibleCards[0].id).classList.remove("card-show");
+                    visibleCards = [];
+                },2000);
+            }
+        }
+        
+        // card.classList.toggle("card-show");
+        // setInterval(() =>{card.classList.toggle("card-show")},500);
     });
-};
+});
+
+// const cards = document.querySelectorAll(".card");
+// for (let card of cards)
+// {
+//     card.addEventListener("click", ()=>
+//     {
+//         card.classList.toggle("card-show");
+//         setTimeout(() =>{card.classList.toggle("card-show")},2000);
+//     });
+// };
